b7a8f556065ad1668f39c586d411ff1acb7c1a3c
smith <carsia88@gmail.com>
2017-11-30 20:24:58 +0900

    commit

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,10 @@ from django.utils import  timezone
 from .models import  Post
 from django.shortcuts import render, get_object_or_404
 
+def index(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/index.html',{'posts':posts})
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts':posts})
